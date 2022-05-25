2869b1d75a33d41b799820ead17409adb0e2bccd
Rupace Pakhrin <rupacepakhrin@gmail.com>
2022-05-25 18:27:35 +0545

    first commit

--- src/Message.js
+++ src/Message.js
@@ -5,8 +5,8 @@ import './Message.css';
 const Message = forwardRef(({message, username}, ref) => {
     const isUser = username === message.username;
   return (
-  
-    <div ref={ref} className={'message ${isUser && "message__user"}'}>  
+
+    <div ref={ref} className={`message ${isUser && 'message__user'}`}>  
         <Card className={isUser ? "message__userCard" : "message__guestCard"}>
             <CardContent>
                 <Typography
@@ -14,7 +14,7 @@ const Message = forwardRef(({message, username}, ref) => {
                 variant="h5"
                 component="h2"
                 >
-                    {message.username}: {message.message}
+                    {!isUser && `${message.username || "Unkown"}: `}{message.message}
                 </Typography>
             </CardContent>
         </Card>
